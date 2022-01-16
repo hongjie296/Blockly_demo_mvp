1f543cc2889d0280d72d904e7293a693179cacf4
hongjie296 <75304051+hongjie296@users.noreply.github.com>
2022-01-16 13:15:09 +0800

    Added my project

---
+++ blocks/movement.js
@@ -0,0 +1,34 @@
+
+Blockly.defineBlocksWithJsonArray([{
+  "type": "movement",
+  "message0": "Movement %1",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "NAME",
+      "options": [
+        [
+          "Up",
+          "U"
+        ],
+        [
+          "Down",
+          "D"
+        ],
+        [
+          "Right",
+          "R"
+        ],
+        [
+          "Left",
+          "L"
+        ]
+      ]
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+}])
